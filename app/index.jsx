8baaf1b757ec1ecd9b184f543db331693d89cf83
Matis Tonisson <matistonisson@gmail.com>
2024-11-15 10:14:35 +0200

    Lisasin paddingu kirjetele ja nupule

--- app/index.jsx
+++ app/index.jsx
@@ -16,18 +16,18 @@ const app = () => {
     <ScrollView contentContainerStyle={{ height: '100%' }}>
       <View style={{ justifyContent: 'center', alignItems: 'center', minHeight: '85%', paddingHorizontal: 16 }}>
       
-        <Image source={images.logo} style={{ width: 440, height: 204 }} resizeMode="contain" />
+        <Image source={images.logo} style={{ width: 500, height: 250 }} resizeMode="contain" />
         {/*<Image source={images.cards} style={{ maxWidth: 380, width: '100%', height: 300 }} resizeMode="contain" />*/}
-        <View style={{ marginTop: 20, alignItems: 'center' }}>
-          <Text style={{ fontSize: 24, color: '#FFFFFF', fontWeight: 'bold', textAlign: 'center' }}>
-          Sinu finantselu,<Text style={{ color: '#F65C06'}}>sinu kontrolli all</Text> 
+        <View style={{ marginTop: 20, alignItems: 'center', marginBottom:30 }}>
+          <Text style={{ fontSize: 32, color: '#FFFFFF', fontWeight: 'bold', textAlign: 'center', lineHeight: 50 }}>
+          Sinu finantselu,{"\n"}<Text style={{ color: '#F65C06'}}>Sinu kontrolli all</Text> 
           </Text>
         </View>
         
         <CustomButton 
           title="Liigu edasi" 
           handlePress={() => router.push('/sign-in')}  
-          containerStyles={{ width: '100%', paddingTop:20, color:'#fff'}} 
+          containerStyles={{ width: '100%', color:'#fff', fontSize:32}} 
         />
       </View>
     </ScrollView>
